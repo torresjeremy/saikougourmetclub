c58119893d4fb7030492bc62d5ab0d15a2857fa4
torresjeremy <torresjeremyrenzo@gmail.com>
2022-02-12 21:41:30 +0800

    Added scroll to top on menu page init

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,33 +1,40 @@
 /* eslint-disable react/jsx-one-expression-per-line */
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   Flex, Center, VStack, Box, Text,
 } from '@chakra-ui/react';
 import HeaderContact from 'images/contact/header_contact.png';
 
-const Contact: React.FC = () => (
-  <Flex direction="row">
-    <VStack width="100%" bgColor="yellow.150">
-      <Box
-        backgroundImage={`url(${HeaderContact})`}
-        backgroundSize="cover"
-        backgroundRepeat="no-repeat"
-        w="100%"
-        height={{ base: '80vh' }}
-      >
-        <Center height="85%">
-          <Box
-            fontFamily="CFSamuraiBob"
-            fontWeight="400"
-            color="red.150"
-            fontSize="7xl"
-          >
-            Serving Real Soon <Text fontFamily="GangOfThree" display="inline" fontSize={{ base: '5xl' }}>!</Text>
-          </Box>
-        </Center>
-      </Box>
-    </VStack>
-  </Flex>
-);
+const Contact: React.FC = () => {
+  useEffect(() => {
+    console.log('scroll to top');
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <Flex direction="row">
+      <VStack width="100%" bgColor="yellow.150">
+        <Box
+          backgroundImage={`url(${HeaderContact})`}
+          backgroundSize="cover"
+          backgroundRepeat="no-repeat"
+          w="100%"
+          height={{ base: '80vh' }}
+        >
+          <Center height="85%">
+            <Box
+              fontFamily="CFSamuraiBob"
+              fontWeight="400"
+              color="red.150"
+              fontSize="7xl"
+            >
+              Serving Real Soon <Text fontFamily="GangOfThree" display="inline" fontSize={{ base: '5xl' }}>!</Text>
+            </Box>
+          </Center>
+        </Box>
+      </VStack>
+    </Flex>
+  );
+};
 
 export default Contact;
